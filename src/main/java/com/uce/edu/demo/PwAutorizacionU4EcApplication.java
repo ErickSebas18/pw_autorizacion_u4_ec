4953a0a62e212c7735b6b3213447dec4a6ca7b07
Erick Chávez <ericksebastianchavezgarcia4gmail.com>
2023-08-09 19:46:55 -0500

    Main

---
+++ src/main/java/com/uce/edu/demo/PwAutorizacionU4EcApplication.java
@@ -0,0 +1,13 @@
+package com.uce.edu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwAutorizacionU4EcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwAutorizacionU4EcApplication.class, args);
+	}
+
+}
